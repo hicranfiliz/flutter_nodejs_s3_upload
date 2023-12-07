c08f63c3e07cea843fc80bcfc982342e3a3cf8e3
hicranfiliz <h.filiz781@gmail.com>
2023-12-07 11:47:38 +0300

    added to upload files to aws backend

---
+++ api/config/s3.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    AWS_ACCESS_KEY_ID : "AKIAUBLBADQTTK4J3HXS",
+    AWS_SECRET_ACCESS_KEY : "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",
+    AWS_BUCKET_NAME: "the-unique-bucket-1",
+    AWS_REGION : "eu-north-1"
+}
